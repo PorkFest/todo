f4107564789eaae9a28807be738c527c908dd3c3
PorkFest <juho.kujala@outlook.com>
2020-10-31 11:13:08 +0200

    tehtävä 2

--- app/Controllers/Todo.php
+++ app/Controllers/Todo.php
@@ -8,7 +8,9 @@ class Todo extends BaseController
 {
     public function index()
     {
+        $model = new TodoModel();
         $data['title'] = 'Todo';
+        $data['todos'] = $model->getTodos();
         echo view('templates/header');
         echo view('todo/list', $data);
         echo view('templates/footer');
